707dfa38bb75755f01345a56e38121b3c66d1817
datadrivensupplychain <76882555+datadrivensupplychain@users.noreply.github.com>
2021-10-20 09:54:04 -0500

    Add files via upload

--- monty_hall.r
+++ monty_hall.r
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(magrittr)
 
-simulation_count <- 10000000
+simulation_count <- 1000000
 
 door_df <- cbind.data.frame(
 #randomly assign door with prize
@@ -13,11 +13,15 @@ Door_You_Initially_Choose = sample(x=c('A','B','C'),size=simulation_count,replac
 stringsAsFactors=FALSE) %>%
 
 #new column: would it be better to stay (the Door_With_Prize is the same as Door_You_Initially_Choose)
-  #or to switch?
+#or to switch?
+
   
 dplyr::mutate(
   Stay_or_Switch = dplyr::if_else(Door_With_Prize==Door_You_Initially_Choose,"Stay","Switch"))
 
+
+#summarize results of the simulation: how often would it be better to Stay or to Switch?
+#results confirm: 2/3rds of the time, better to switch.  Switching is a better strategy.
 results <- door_df %>%
   dplyr::group_by(Stay_or_Switch) %>%
   dplyr::summarise(Count = dplyr::n()) %>%
