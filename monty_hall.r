e2d10a5661d200743be385f211bf00fc9883faa9
datadrivensupplychain <76882555+datadrivensupplychain@users.noreply.github.com>
2021-10-20 09:38:55 -0500

    Add files via upload

---
+++ monty_hall.r
@@ -0,0 +1,27 @@
+#Monty Hall Simulation
+
+library(tidyverse)
+library(magrittr)
+
+simulation_count <- 10000000
+
+door_df <- cbind.data.frame(
+#randomly assign door with prize
+Door_With_Prize = sample(x=c('A','B','C'),size=simulation_count,replace=TRUE,prob=c(1/3,1/3,1/3)),
+#randomly assign the door you initially choose
+Door_You_Initially_Choose = sample(x=c('A','B','C'),size=simulation_count,replace=TRUE,prob=c(1/3,1/3,1/3)),
+stringsAsFactors=FALSE) %>%
+
+#new column: would it be better to stay (the Door_With_Prize is the same as Door_You_Initially_Choose)
+  #or to switch?
+  
+dplyr::mutate(
+  Stay_or_Switch = dplyr::if_else(Door_With_Prize==Door_You_Initially_Choose,"Stay","Switch"))
+
+results <- door_df %>%
+  dplyr::group_by(Stay_or_Switch) %>%
+  dplyr::summarise(Count = dplyr::n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(Pct_Correct_Choice =scales::percent(Count/sum(Count) ))
+
+results
