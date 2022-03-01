4b995ac4181e3a4057b4c29cb3af37d44e29935f
datadrivensupplychain <76882555+datadrivensupplychain@users.noreply.github.com>
2022-03-01 11:33:56 -0600

    Delete date_parsing.r

--- date_parsing.r
+++
@@ -1,7 +0,0 @@
-datevec <- c('2021-1-3','12-1-2022')
-mydate <- datevec %>% as.data.frame() %>% 
-  dplyr::mutate(date = dplyr::case_when(
-  is.na(lubridate::ymd(.))==FALSE ~ lubridate::ymd(.),
-  is.na(lubridate::mdy(.))==FALSE ~ lubridate::mdy(.),
-  TRUE ~ NULL))
-#doesn't work yet!
