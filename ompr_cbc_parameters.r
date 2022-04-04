29fb62a251326584a0412adac55c735deb65a11d
datadrivensupplychain <76882555+datadrivensupplychain@users.noreply.github.com>
2022-04-03 21:16:26 -0500

    Update ompr_cbc_parameters.r

--- ompr_cbc_parameters.r
+++ ompr_cbc_parameters.r
@@ -15,6 +15,8 @@ model <- ompr::MILPModel()  %>%
   add_constraint(x + y <= 11.25)
 
 model_soln <- model %>%   solve_model(with_ROI(solver = "cbc",
-max_time=100,verbosity_level=1,ratioGap=0.4))
+max_time=100,verbosity_level=1,ratioGap=0.4,feas="both"))
 #verbosity_level = -1 (no verbosity). 1 or 2 gives verbosity.
-#max_time is in seconds. ratioGap is in percentx100, so 0.4 is 40% gap.
+#max_time is in seconds. ratioGap is in percentx100, so 0.4 is 40% gap between
+#incumbent solution and current best possible solution
+#feas is feasibility pump heursitic https://projects.coin-or.org/Cbc/wiki/fpump
